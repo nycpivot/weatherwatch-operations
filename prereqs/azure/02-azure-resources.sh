83a80e00d9fefdbe6b6faa62620a36634744c347
James <Michael.James02@mmc.com>
2024-03-15 18:22:53 -0400

    Increases node count to 2.

--- prereqs/azure/02-azure-resources.sh
+++ prereqs/azure/02-azure-resources.sh
@@ -12,7 +12,7 @@ az group create --name ${resource_group} --location ${aks_region_code}
 
 # create aks cluster
 az aks create --name ${cluster_name} --resource-group ${resource_group} \
-	    --node-count 1 --node-vm-size Standard_B2ms --kubernetes-version 1.28.5 \
+	    --node-count 2 --node-vm-size Standard_B2ms --kubernetes-version 1.28.5 \
 	        --enable-managed-identity --enable-addons monitoring --enable-msi-auth-for-monitoring --generate-ssh-keys 
 
 az aks get-credentials --name ${cluster_name} --resource-group ${resource_group}

fd71472dd6226c5ed404f9f88d81f14766381831
James <Michael.James02@mmc.com>
2024-03-17 18:59:05 -0400

    Fixes azure storage account.

--- prereqs/azure/03-storage-create.sh
+++ prereqs/azure/03-storage-create.sh
@@ -4,15 +4,15 @@ resource_group=weatherwatch
 storage_account=weatherwatch$RANDOM
 container_name=extreme-temps
 
-# save storage account name (random)
-echo $storage_account > ~/weatherwatch-dapr-ops/az-storage-account.txt
-
 # create storage account and container
 az storage account create --name $storage_account --resource-group $resource_group
 
 az storage container create --name $container_name --account-name $storage_account
 
 account_key=$(az storage account keys list --account-name $storage_account | jq -r .[0].value)
+
+# save storage account name (random)
+echo $storage_account > ~/tmp/az-storage-account.txt
 
 # save storage account key for dapr component
 echo $account_key > ~/tmp/az-storage-account-key.txt

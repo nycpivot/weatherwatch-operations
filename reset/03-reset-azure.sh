8109e9fc8f9d388595f3750b64857b914e6dbc93
James <Michael.James02@mmc.com>
2024-03-18 10:34:02 -0400

    Fixed azure reset.

--- reset/03-reset-azure.sh
+++ reset/03-reset-azure.sh
@@ -6,7 +6,7 @@ cluster_name=weatherwatch-app
 
 storage_account=$(cat ~/tmp/az-storage-account.txt)
 
-az servicebus namespace delete --namespace $asb_namespace --resource-group $resource_group -y
+az servicebus namespace delete --namespace $asb_namespace --resource-group $resource_group
 az storage account delete --name $storage_account --resource-group $resource_group -y
 az aks delete --name $cluster_name --resource-group $resource_group -y
 
